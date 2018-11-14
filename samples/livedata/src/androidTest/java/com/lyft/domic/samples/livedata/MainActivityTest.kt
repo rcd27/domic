cc04b274306a60280a34e1eace2b4f9263328b66
Zemlyakov Stas <redtom@yandex.ru>
2018-11-14 09:56:25 +0300

    Copy `mvvm` sample to implement `livedata`. Add android test dependencies, instrumentation test and TODO

---
+++ samples/livedata/src/androidTest/java/com/lyft/domic/samples/livedata/MainActivityTest.kt
@@ -0,0 +1,19 @@
+package com.lyft.domic.samples.livedata
+
+import org.junit.Test
+
+class MainActivityTest {
+
+    @Test
+    fun mainCase() {
+        // Given activity is running
+
+        // When it goes to background
+
+        // ViewModel emits data
+
+        // Activity restores its active state
+
+        // Check that all data is shipped to the view
+    }
+}
---
+++ samples/livedata/src/main/java/com/lyft/domic/samples/livedata/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.lyft.domic.samples.livedata
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.lyft.domic.android.rendering.AndroidRenderer
+import com.lyft.domic.samples.livedata.signin.AndroidSignInView
+import com.lyft.domic.samples.livedata.signin.SignInViewModel
+import com.lyft.domic.samples.shared.signin.RealSignInService
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.plusAssign
+import io.reactivex.schedulers.Schedulers
+
+class MainActivity : AppCompatActivity() {
+
+    private val disposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val renderer = AndroidRenderer()
+        val view = AndroidSignInView(findViewById(android.R.id.content), renderer)
+
+        disposable += Single
+                .fromCallable { SignInViewModel(view, RealSignInService()) }
+                // Showcase that you can observe Virtual DOM on non-main thread.
+                .subscribeOn(Schedulers.computation())
+                .subscribe { viewModel -> disposable += viewModel }
+    }
+
+    override fun onDestroy() {
+        disposable.dispose()
+        super.onDestroy()
+    }
+}
